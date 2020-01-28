7f204a836f40f3430c6b516e28b37427b5833b44
CRIS67 <club-robotique@insa-strasbourg.fr>
2020-01-28 17:47:20 +0100

    ajout prog phare

---
+++ Programmation/Phare/code_carte_phare_Eva/code_carte_phare_Eva.ino
@@ -0,0 +1,79 @@
+#define RX                0
+#define TX                1
+#define state             2
+#define RUPT_INFO_Haut    3
+#define RUPT_INFO_Bas     4
+#define RUPT_INFO_DG      5
+#define RESET_XBEE        6
+#define PWM_Servo0        7
+#define leds              8
+#define pwm_servo1        9
+#define sens_act_Montee   16
+#define sens_act_Tourner  17 
+#define pwm_act_Monter     18
+#define pwm_act_Tourner    19
+
+
+#define monterPhare                   1
+#define descendrePhare                ~monterPhare
+#define tournerLedsGauche             0
+#define tournerLedsDroite             ~tournerLedsGauche
+#define vitesseDeCroisiereMonter      200
+#define vitesseDeCroisiereTourner     200
+
+char receive;
+ 
+void setup() {
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
+  pinMode(state, OUTPUT);
+  pinMode(RUPT_INFO_Haut, INPUT);
+  pinMode(RUPT_INFO_Bas, INPUT);
+  pinMode(RUPT_INFO_DG, INPUT);
+  pinMode(RESET_XBEE, OUTPUT);
+  pinMode(leds, OUTPUT);
+  pinMode(sens_act_Montee, OUTPUT);
+  pinMode(sens_act_Tourner, OUTPUT);
+  pinMode(pwm_act_Monter, OUTPUT);
+  pinMode(pwm_act_Tourner, OUTPUT);
+  digitalWrite(state, 0);
+  digitalWrite(RESET_XBEE, 1);
+  digitalWrite(sens_act_Montee, 0);
+  digitalWrite(sens_act_Tourner, 0);
+  digitalWrite(leds, 0);
+  analogWrite(pwm_act_Monter, 0);
+  analogWrite(pwm_act_Tourner, 0);
+  Serial.begin(9600);
+
+  digitalWrite(sens_act_Montee, descendrePhare);
+  analogWrite(pwm_act_Monter, vitesseDeCroisiereMonter);
+  while(!digitalRead(RUPT_INFO_Bas)) {}
+  analogWrite(pwm_act_Monter, 0);
+}
+
+void loop() {
+  if(Serial.available()) {
+    receive = Serial.read();
+    if(receive == 'A') {
+      digitalWrite(leds, 1);
+      digitalWrite(sens_act_Montee, monterPhare);
+      digitalWrite(sens_act_Tourner, tournerLedsGauche);
+      analogWrite(pwm_act_Monter, vitesseDeCroisiereMonter);
+      analogWrite(pwm_act_Tourner, vitesseDeCroisiereTourner);
+      while(1) {
+        if(digitalRead(RUPT_INFO_DG)) {
+          digitalWrite(sens_act_Tourner, ~digitalRead(sens_act_Tourner));
+        }
+        if(digitalRead(RUPT_INFO_Haut)) {
+           analogWrite(pwm_act_Monter, 0);
+        }
+        if(Serial.available()) {
+          receive = Serial.read();
+          if(receive == 'E') {
+              
+          }
+        }
+      }
+    }
+  }
+}
