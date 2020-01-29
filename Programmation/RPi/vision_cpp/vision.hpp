916d9760f45bedbd99048f0619d3aa83b0897479
Mehdkit <55589811+Mehdkit@users.noreply.github.com>
2020-01-29 12:05:45 +0100

    Version qui marche sur le robot
    Correction de bug :
    Erreur segfault corrigé dans le makefile : je compilais avec opencv-lib mais je n'avais pas inclue open-cv (la librairie principale)
    Erreur dans EstimatePoseSingleMarkers : le format du vecteur corners était mauvais (il faut le vecteur des vecteurs associées à chaque markers qui regroupe les 4 coins de ceux-ci)
    
    Reste à faire : 
    ajuster le critère de détection NORD/SUD lorsque la caméra sera fixée sur le robot

--- Programmation/RPi/vision_cpp/vision.hpp
+++ Programmation/RPi/vision_cpp/vision.hpp
@@ -2,21 +2,12 @@
 #define VALEUR_TAG_GIROUETTE 17
 #define ERR 424242
 
-#include <string>
-#include <stdio.h>
- #include <ctime>
-#include <iostream>
-
 //Include des biblio pour l'objet vision
-#include </usr/local/include/opencv2/aruco.hpp>
-#include </usr/local/include/opencv2/opencv.hpp>
-#include </usr/local/include/opencv2/highgui/highgui.hpp>
-#include </usr/local/include/opencv2/videoio.hpp>
-#include </usr/local/include/opencv2/imgproc/imgproc.hpp>
+#include <opencv2/aruco.hpp>
+#include <string>
+#include <opencv2/opencv.hpp>
 #include <raspicam/raspicam_cv.h>
-#include <raspicam/raspicam.h>
 
-//using namespace std;
 
 enum Orientation
 {
@@ -28,18 +19,18 @@ typedef Orientation Orientation;
 class Vision
 {
     public:
-        void init_vision();
-        void prendre_photo(std::string nom);
-        void stop_vision();
+    void init_vision();
 		Orientation recuperer_sens_girouette();
-		
+		void prendre_photo(std::string name);
+    void stop_vision();
 		
     protected:
 		raspicam::RaspiCam_Cv Camera;
-		cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-		cv::Ptr<cv::aruco::DetectorParameters> params = cv::aruco::DetectorParameters::create();
-		cv::Mat cameraMatrix;
-		cv::Mat distCoeffs;
-		std::vector<cv::Vec3d> rvecs, tvecs;
+		cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_250);
+    float matDist[3][3] = {{11166.9065,0,393.296681},{0, 11166.9065, 712.958149},{0, 0, 1}};
+    float Coeff[14][1] = {{39.8681247},{-27052.856},{1.95980792},{-1.17693528},{92079.8228},{-47.3561629},{-13565.8581},{-106854.607},{0},{0},{0},{0},{0},{0}};
+		cv::Mat distCoeffs = cv::Mat(14,1,CV_32F,Coeff);
+		cv::Mat cameraMatrix = cv::Mat(3,3,CV_32F,matDist);
+		
     private:
 };
